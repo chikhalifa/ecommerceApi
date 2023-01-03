4686bb76f813b2f5a57bbe8e3dbca3a433fdef82
chikhalifa <49573718+chikhalifa@users.noreply.github.com>
2023-01-03 21:14:09 +0100

    added api resoureces

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -50,7 +50,7 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         //
-        return $product;
+       // return $product;
         return  new ProductResource($product);
     }
 
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Resources\Product;
+use App\Models\Model\Review;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,12 +16,19 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
        // return parent::toArray($request);
+       $test=\App\Models\Model\Review::find($this->id);
+      // dd($test->star);
        return [
         'name' => $this->name,
         'description' =>$this->detail,
         'price' => $this->price,
         'stock'=> $this->stock,
-        'discount'=> $this->discount
+        'discount'=> $this->discount,
+        'rating'=>$test->star?round($test->star,2):'no rating',
+       
+        'href' =>[
+        'reviews'=>route('reviews.index',$this->id)
+        ]
        ];
     }
 }
